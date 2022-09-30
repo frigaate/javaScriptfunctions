88f62b3551f739f31903b8f2ba0db593090d4b0f
riGGed <local.riGGed@email.com>
2022-09-30 14:23:12 +0100

    Introduction to objects in JavaScript

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -7,3 +7,23 @@ const arr1 = [3, 0, 1, 2]
 // arr.shift()
 
 console.log(arr)
+
+const person = {
+    name: "Akpos", age: 46, isMarried: false
+}
+
+// To access any property of an array we use
+// dot notation 
+person.name
+// or reassign it with 
+person.name = "Olusegun"
+// We can add property and value to an object by using
+person.yearOfBirth = 1976
+//An object value can also be retrieved or assigned with bracket notation as
+person["obj"]
+//To access a value, the property inside the  square beacket, must be a string
+//To add property and value can also be done with the square bracket by
+person["is cute"] = false
+//When a peoperty is creater with space between, it must always be referenced with square bracket method and quotation mark, because of the space, as using the dot method will not recognise the space
+
+console.log(person)
