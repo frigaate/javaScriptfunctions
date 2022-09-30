be084a8267dfecbd333225407a8dbdeb95ed5b56
riGGed <local.riGGed@email.com>
2022-09-30 13:51:38 +0100

    Played around arrays, push, pop, unshift and shift concepts

---
+++ Assets/Js/app.js
@@ -0,0 +1,9 @@
+const arr = [4, 9, 10, 20]
+const arr1 = [3, 0, 1, 2]
+
+// arr.push(25, 23, 90, "hello", true, 2.264, ["Nice", true, 3.142])
+// arr.unshift()
+// arr.pop()
+// arr.shift()
+
+console.log(arr)
