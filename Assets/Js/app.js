40fdd1948daeef9b3537ff8a0d19760935c6171c
riGGed <local.riGGed@email.com>
2022-10-12 15:11:54 +0100

    Basics of functions

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -1,76 +1,58 @@
-const arr = [4, 9, 10, 20]
-const arr1 = [3, 0, 1, 2]
+//Methods of creating functions in JavaScript
+//Function call is always the same no matter the declarative method used
+//Function Declaration mode
+function saayHello(){    
+    console.log("Hello World");
+}
+saayHello();
 
-// arr.push(25, 23, 90, "hello", true, 2.264, ["Nice", true, 3.142])
-// arr.unshift()
-// arr.pop()
-// arr.shift()
-
-console.log(arr)
-
-const person = {
-    name: "Akpos", age: 46, isMarried: false
+let user = "Wisdom"
+function sayHello(name){    
+    console.log("Hello", name);
 }
 
-// To access any property of an array we use
-// dot notation 
-person.name
-// or reassign it with 
-person.name = "Olusegun"
-// We can add property and value to an object by using
-person.yearOfBirth = 1976
-//An object value can also be retrieved or assigned with bracket notation as
-person["obj"]
-//To access a value, the property inside the  square beacket, must be a string
-//To add property and value can also be done with the square bracket by
-person["is cute"] = false
-//When a property is creater with space between, it must always be referenced with square bracket method and quotation mark, because of the space, as using the dot method will not recognise the space
+sayHello("Dave");
+sayHello("John");
+sayHello(user);
+sayHello();
 
-console.log(person)
+// A rest operator is used in javascript for functions without a defined numbers of parameters
 
-// const scores = [22, 54, 76, 92, 43, 33];
-// console.log(scores.length);
-
-// let i = 0;
-// while(i < scores.length){
-//     console.log(scores[i]+2);
-//     i++;
-// }
-
-// do{
-//     consolee.log(scores[i]);
-//     i++;
-// }
-// while(i < scores.length);
-
-const score = [1, 2, 3, 4, 5, 6];
-
-for(i = 0; i < score.length; i++)
-{
-    console.log(score[i] + 2);
+function calcAverage(...x){
+    console.log(x);
 }
 
+calcAverage(1, 2, 3, 4, 5);
 
+function addNumber(x, y){
+    const z = x + y ;
+    return  [z, x];
+}
+const result = addNumber(9, 1);
+console.log(result)
 
-//Sort even and odd numbers
-const numbers = [];
-const even = [];
-const odd = [];
+//Function Expression
+const nice = function(){
+    console.log("nice");
+}
+nice();
 
-for(let i = 0; i < 20; i++){
-    numbers[i] = i + 1;
+// Arrow syntax
+const fresh = () => {
+    console.log("fresh");
 }
 
-for(let i = 0; i < numbers.length; i++){
+fresh()
 
-    if(numbers[i] % 2 == 0){
-        even.push(numbers[i]);
-    }
-
-    else{
-        odd.push(numbers[i]);
-    }
-
+//When using a single parameter you can choose to not put it in any parenthesis
+const sayHello2 = (name) => {
+    console.log("Hello", name);
 }
+sayHello2("Jake");
 
-console.log(even, odd);
+//When using the arrow syntax, you can choose to remove curly braces if it's just one statement, but  the value of that expression will be returned automatically
+const addNumber2 = (x, y) => x + y
+console.log(addNumber2(2, 3));
+
+const cube = x => x * x * x
+console.log(cube(5));
